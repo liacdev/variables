a84ff05aa87fb70ae394151d8a4f8c9664395431
liacdev <jagoblacksky@gmail.com>
2022-10-30 20:24:17 -0500

    First commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+    
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    
+    println!("The value of Three Hours In Seconds is: {THREE_HOURS_IN_SECONDS}");
+    println!("");
+    println!("Shadowing");
+
+
+}
